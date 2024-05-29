a2b66101bb505c033631b711e8492b816a2ea31b
protarie125 <protarie@gmail.com>
2024-05-29 21:17:03 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <bit>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = int64_t;
+using vl = vector<ll>;
+
+ll a, p, c;
+
+int main() {
+  ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  cin >> a >> p >> c;
+
+  cout << max(a + c, p);
+
+  return 0;
+}
